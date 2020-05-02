1526340d19a1ee1798aff23ccba86128030c6229
Junaid Choudhary <junaidchoudhary210@gmail.com>
2020-05-02 22:55:19 +0530

    home page test variable added

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 import dotenv from "dotenv"
 dotenv.config()
 export const connectionString = process.env.CONNECTION_STRING
+export const testEnvironmentVariable = process.env.TEST_ENVIRONMENT_VARIABLE
